f684d8c2aa0b20ab3d0e932afa40b3eb592ca9d8
NIk <a4329880@gmail.com>
2025-06-29 14:07:25 +0200

    Add main

---
+++ main.py
@@ -0,0 +1,14 @@
+import lmstudio as lms
+SERVER_API_HOST = "localhost:1234"
+lms.configure_default_client(SERVER_API_HOST)
+
+def multiply(a: float, b: float) -> float:
+    """Given two numbers a and b. Returns the product of them."""
+    return a * b
+
+model = lms.llm("qwen2.5-7b-instruct")
+model.act(
+  "What is the result of 12345 multiplied by 54321?",
+  [multiply],
+  on_message=print,
+)
